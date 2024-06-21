4299a33bb5820231c8a289bda29ccaf9120fafc5
Alano-i <522081732@qq.com>
2024-06-21 18:55:54 +0800

    字体

--- frontend/src/layout/components/header.tsx
+++ frontend/src/layout/components/header.tsx
@@ -2,7 +2,7 @@ const Header: React.FC = () => {
   return (
     <div className="flex gap-2 items-center mb-8">
       <img src="/img/logo.svg" alt="logo" className="w-[32px] h-[32px]" />
-      <div className="text-[18px] text-[#ffffffaa]" style={{ fontFamily: 'Poppins, sans-serif', fontWeight: 900 }}>115FileLister</div>
+      <div className="text-[18px] text-[#ffffffaa]" style={{ fontFamily: 'Poppins, sans-serif', fontWeight: 900 }}>115 FileLister</div>
     </div>
   );
 };
