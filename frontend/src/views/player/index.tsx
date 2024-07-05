02520d09c28a82e6efe0f45df7b405223510fcd6
zkl <i@zkl2333.com>
2024-07-05 14:47:11 +0800

    feat: 🎸 画质选择

--- frontend/src/views/player/index.tsx
+++ frontend/src/views/player/index.tsx
@@ -2,62 +2,80 @@ import { useEffect, useRef } from "react";
 import Artplayer from "artplayer";
 import { useSearchParams } from "react-router-dom";
 import Hls from "hls.js";
-import artplayerPluginHlsQuality from "artplayer-plugin-hls-quality";
 
 const Player = () => {
   let [searchParams, _] = useSearchParams();
 
   const pickcode = searchParams.get("pickcode");
+  const realType = searchParams.get("real_type");
 
-  const artRef = useRef(null);
+  const artRef = useRef<HTMLDivElement>(null);
+  const artInstanceRef = useRef<Artplayer | null>(null);
 
-  const option = {
-    url: `/api/m3u8?pickcode=${pickcode}`,
-    // url: "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8",
-    type: "m3u8",
-    setting: true,
-    fullscreen: true,
-    fullscreenWeb: true,
-    plugins: [
-      artplayerPluginHlsQuality({
-        // Show quality in control
-        control: false,
+  const createArtOption = () => {
+    const art = artInstanceRef.current!;
+    const container = artRef.current!;
 
-        // Show quality in setting
-        setting: true,
-
-        // Get the resolution text from level
-        getResolution: (level) => {
-          console.info(level);
-          if (level.height <= 0) return "默认";
-          return level.height + "P";
+    const option: Artplayer["Option"] = {
+      container: container,
+      setting: true,
+      fullscreen: true,
+      fullscreenWeb: true,
+      url: `/api/m3u8?pickcode=${pickcode}&definition=3`,
+      type: "m3u8",
+      settings: [
+        {
+          html: "画质",
+          width: 150,
+          selector: [
+            {
+              html: "转码 HD",
+              url: `/api/m3u8?pickcode=${pickcode}&definition=3`,
+              type: "m3u8",
+            },
+            {
+              html: "转码 UD",
+              url: `/api/m3u8?pickcode=${pickcode}&definition=4`,
+              type: "m3u8",
+            },
+            {
+              html: "直连 原始画质",
+              url: `/api/download?pickcode=${pickcode}`,
+              type: realType,
+            },
+          ],
+          onSelect: function (item, $dom, event) {
+            console.info(item, $dom, event);
+            art.switchQuality(item.url);
+            art.type = item.type;
+            art.notice.show = "切换画质: " + item.html;
+            return item.html;
+          },
         },
-
-        // I18n
-        title: "质量",
-        auto: "自动",
-      }),
-    ],
-    customType: {
-      m3u8: function playM3u8(
-        video: HTMLMediaElement,
-        url: string,
-        art: Artplayer
-      ) {
-        if (Hls.isSupported()) {
-          if (art.hls) art.hls.destroy();
-          const hls = new Hls();
-          hls.loadSource(url);
-          hls.attachMedia(video);
-          art.hls = hls;
-          art.on("destroy", () => hls.destroy());
-        } else if (video.canPlayType("application/vnd.apple.mpegurl")) {
-          video.src = url;
-        } else {
-          art.notice.show = "不支持播放格式: m3u8";
-        }
+      ],
+      customType: {
+        m3u8: function playM3u8(
+          video: HTMLMediaElement,
+          url: string,
+          art: Artplayer
+        ) {
+          if (Hls.isSupported()) {
+            if (art.hls) art.hls.destroy();
+            const hls = new Hls();
+            hls.loadSource(url);
+            hls.attachMedia(video);
+            art.hls = hls;
+            art.on("destroy", () => hls.destroy());
+          } else if (video.canPlayType("application/vnd.apple.mpegurl")) {
+            video.src = url;
+          } else {
+            art.notice.show = "不支持播放格式: m3u8";
+          }
+        },
       },
-    },
+    };
+
+    return option;
   };
 
   const getInstance = (art: Artplayer) => console.info(art);
@@ -67,10 +85,8 @@ const Player = () => {
       return;
     }
 
-    const art = new Artplayer({
-      ...option,
-      container: artRef.current,
-    });
+    const art = new Artplayer(createArtOption());
+    artInstanceRef.current = art;
 
     art.on("error", (error, reconnectTime) => {
       console.info(error, reconnectTime);
@@ -93,7 +109,8 @@ const Player = () => {
 
   return (
     <div className="md:px-20 h-full flex justify-center items-center rounded-[50px]">
-      <div className="w-full aspect-video rounded-[50px]"
+      <div
+        className="w-full aspect-video rounded-[50px]"
         // 播放器的尺寸依赖于容器 container 的尺寸，所以你的容器 container 必须是有尺寸的
         // style={{
         //   width: "100%",
