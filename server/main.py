cf7d4a9e93bdb61f1286234df1210f32090178dd
chenyanggao <2339083510@qq.com>
2024-07-23 12:38:07 +0800

    update.1721709487

--- server/main.py
+++ server/main.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python3
 # encoding: utf-8
 
-import uvicorn
 import os
-from server.file_lister import *
-# 静态文件服务：https://www.neoteroi.dev/blacksheep/static-files/
-app.serve_files("./frontend")
+
 
 def echo_author():
     docker_version = os.environ.get("VERSION_115_FILE_LISTER")
@@ -20,13 +17,22 @@ def echo_author():
                 """)
     print(f"Version: {docker_version}")
 
+
 def startup():
     echo_author()
     print(f'程序启动成功')
 
-def main():
-    startup()
-    uvicorn.run(app, host="0.0.0.0", port=9115, reload=False, proxy_headers=True, forwarded_allow_ips="*")
 
-if __name__ == '__main__':
+def main():
+    from server.file_lister import main
+    startup()
     main()
+
+
+if __name__ == "__main__":
+    from pathlib import Path
+    from sys import path
+
+    path[0] = str(Path(__file__).parents[1])
+    main()
+
