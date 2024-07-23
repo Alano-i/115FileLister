53fe2e7cdcb5d21bb42e0b63b3c6e90ccfb121b9
zkl <i@zkl2333.com>
2024-07-23 15:47:45 +0800

    feat: 🎸 通过 .env 文件注入环境变量

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 import tailwindcss from "tailwindcss";
@@ -12,43 +12,47 @@ function pathResolve(dir: string) {
 }
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  root: "./",
-  base: "/",
-  publicDir: "public",
-  plugins: [svgr(), react()],
-  resolve: {
-    alias: [
-      {
-        find: /\/@\//,
-        replacement: pathResolve("src") + "/",
-      },
-    ],
-  },
-  server: {
-    host: true,
-    port: 3000,
-    // open: true,
-    proxy: {
-      "/api": {
-        target: "http://127.0.0.1",
-        changeOrigin: true,
-        secure: false,
-      },
-      "/ws": {
-        target: "ws://127.0.0.1",
-        ws: true,
+export default defineConfig(({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return {
+    root: "./",
+    base: "/",
+    publicDir: "public",
+    plugins: [svgr(), react()],
+    resolve: {
+      alias: [
+        {
+          find: /\/@\//,
+          replacement: pathResolve("src") + "/",
+        },
+      ],
+    },
+    server: {
+      host: true,
+      port: 3000,
+      // open: true,
+      proxy: {
+        "/api": {
+          target: process.env.VITE_API_URL,
+          changeOrigin: true,
+          secure: false,
+        },
+        "/ws": {
+          target: "ws://127.0.0.1",
+          ws: true,
+        },
       },
     },
-  },
-  build: {
-    target: "es2015",
-    cssTarget: "chrome80",
-    outDir: "dist",
-  },
-  css: {
-    postcss: {
-      plugins: [postcssImport, tailwindcssNesting, tailwindcss, autoprefixer],
+    build: {
+      target: "es2015",
+      cssTarget: "chrome80",
+      outDir: "dist",
     },
-  },
+    css: {
+      postcss: {
+        plugins: [postcssImport, tailwindcssNesting, tailwindcss, autoprefixer],
+      },
+    },
+  };
 });
